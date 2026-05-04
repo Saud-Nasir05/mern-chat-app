d07ef5355c8a6fe09b973572ca9c21f0095e97a3
Saud Nasir <saudnasir05@gmail.com>
2026-05-04 16:02:10 +0500

    cors issue done in socket

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -1,30 +1,43 @@
-    import dotenv from 'dotenv'
-    dotenv.config()
-    import express from "express"
-    import http from "http"
-    import {Server} from "socket.io"
-    const app=express();
-    const server =http.createServer(app)
-    const io =new Server(server,{
-        cors:{
-            origin:'http://localhost:5173'
-        }
-    })
-    const userSocketMap={
+import dotenv from 'dotenv'
+dotenv.config()
+import express from "express"
+import http from "http"
+import {Server} from "socket.io"
 
+const app = express();
+const server = http.createServer(app)
+
+// 🟢 FIX: Yahan Vercel ka naya link daal diya hai array ke andar
+const io = new Server(server, {
+    cors: {
+        origin: [
+            'http://localhost:5173',
+            'https://mern-chat-app-three-mu.vercel.app' // Tumhara naya Vercel Domain!
+        ],
+        methods: ["GET", "POST"], // Yeh laazmi add karo taake requests block na hon
+        credentials: true
     }
-    io.on("connection", (socket) => {
-        const userId=socket.handshake.query.userId;
-        console.log(userId)
-        if(!userId) return
-        userSocketMap[userId]=socket.id
-        io.emit("onlineUsers",Object.keys(userSocketMap))
-        socket.on("disconnect",()=>{
-            delete userSocketMap[userId]
-            io.emit("onlineUsers",Object.keys(userSocketMap))
-        })
+})
+
+const userSocketMap = {}
+
+io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+    console.log(userId)
+    
+    if(!userId) return
+    
+    userSocketMap[userId] = socket.id
+    io.emit("onlineUsers", Object.keys(userSocketMap))
+    
+    socket.on("disconnect", () => {
+        delete userSocketMap[userId]
+        io.emit("onlineUsers", Object.keys(userSocketMap))
     })
-    const getSocketId=(userId)=>{
-        return userSocketMap[userId]
-    }
-    export {io,app,server,getSocketId}
+})
+
+const getSocketId = (userId) => {
+    return userSocketMap[userId]
+}
+
+export { io, app, server, getSocketId }
